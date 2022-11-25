51215fe1b5a7f243b5a7c85e9bd90a18d77e3d75
angel.herce <angel.herce@imatia.com>
2022-11-25 09:48:00 +0100

    Se ha creado el componente Home del módulo de sucursales

--- src/app/main/branches/branches.module.ts
+++ src/app/main/branches/branches.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { BranchesRoutingModule } from './branches-routing.module';
+import { BranchesHomeComponent } from './branches-home/branches-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BranchesHomeComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
