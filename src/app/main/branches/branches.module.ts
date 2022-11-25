6fc5fe89836f303994a190f0546ad76679f080aa
angel.herce <angel.herce@imatia.com>
2022-11-25 09:45:37 +0100

    Se ha creado el módulo de sucursales

---
+++ src/app/main/branches/branches.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { BranchesRoutingModule } from './branches-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OntimizeWebModule,
+    BranchesRoutingModule
+  ]
+})
+export class BranchesModule { }
