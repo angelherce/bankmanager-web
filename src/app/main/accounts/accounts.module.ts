f5c258cc9b1a96386df0bf321bd23af04ccfe697
angel.herce <angel.herce@imatia.com>
2022-11-25 09:56:42 +0100

    Se ha creado el componente Home del módulo de cuentas

--- src/app/main/accounts/accounts.module.ts
+++ src/app/main/accounts/accounts.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { AccountsRoutingModule } from './accounts-routing.module';
+import { AccountsHomeComponent } from './accounts-home/accounts-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AccountsHomeComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
