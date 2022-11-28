32e4cd1227c9fd5adefb396bb15a925c644c1403
angel.herce <angel.herce@imatia.com>
2022-11-28 10:41:26 +0100

    Se ha corregido un error

--- src/app/shared/app.menu.config.ts
+++ src/app/shared/app.menu.config.ts
@@ -10,11 +10,11 @@ export const MENU_CONFIG: MenuRootItem[] = [
   {
     id: 'modules', name: 'MODULES', icon: 'remove_red_eye', opened: true,
     items: [
-      { id: 'customers', name: 'CUSTOMERS', icon: 'people', route: '/main/customers' },
-      { id: 'employees', name: 'EMPLOYEES', icon: 'business_center', route: '/main/employees'},
-      { id: 'branches', name: 'BRANCHES', icon: 'account_balance', route: '/main/branches' },
-      { id: 'accounts', name: 'ACCOUNTS', icon: 'credit_card', route: '/main/accounts' },
-      { id: 'serviceEx', name: 'SERVICEEX', icon: 'dns', route: '/main/serviceEx' }
+      { id: 'customers', name: 'CUSTOMERS', icon: 'people', route: '/main/customers', component: CustomerCardComponent },
+      { id: 'employees', name: 'EMPLOYEES', icon: 'business_center', route: '/main/employees', component: EmployeeCardComponent},
+      { id: 'branches', name: 'BRANCHES', icon: 'account_balance', route: '/main/branches', component: BranchCardComponent },
+      { id: 'accounts', name: 'ACCOUNTS', icon: 'credit_card', route: '/main/accounts', component: AccountCardComponent},
+      { id: 'serviceEx', name: 'SERVICEEX', icon: 'dns', route: '/main/serviceEx', component: ServiceExCardComponent }
     ]
   },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
