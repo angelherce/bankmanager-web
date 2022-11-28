92ab2d09ec828909103896bebad2da9321489c56
angel.herce <angel.herce@imatia.com>
2022-11-28 10:10:55 +0100

    Se ha actualizado la tarjeta del servicio externo

--- src/app/shared/service-ex-card/service-ex-card.component.ts
+++ src/app/shared/service-ex-card/service-ex-card.component.ts
@@ -1,13 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { StarsWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-service-ex-card',
   templateUrl: './service-ex-card.component.html',
-  styleUrls: ['./service-ex-card.component.css']
+  styleUrls: ['./service-ex-card.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    '[class.home-card]': 'true'
+  }
 })
 export class ServiceExCardComponent implements OnInit {
+  filmsAmount: number;
 
-  constructor() { }
+  constructor(
+    private starwars : StarsWarsService,
+    private cd: ChangeDetectorRef
+  ) {
+    this.starwars.query({}, ['uuid'], 'films').subscribe(
+      res => {
+        if (res.data && res.data.length) {
+          this.filmsAmount = res.data.length;
+        }else{
+          this.filmsAmount = undefined;
+        }
+
+        },
+      err => console.log(err),
+      () => this.cd.detectChanges()
+    );
+   }
+
 
   ngOnInit() {
   }
