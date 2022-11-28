b7929e7d54dddf48ea329d7268027f41d7a4ca23
angel.herce <angel.herce@imatia.com>
2022-11-28 09:58:10 +0100

    Se ha añadido el módulo ServiceExModule juntos a sus componentes Home y Details que extraen la información de las películas de Star Wars a partir del servicio que habiamos creado en la carpeta Shared

--- src/app/shared/star-wars.service.ts
+++ src/app/shared/star-wars.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { OntimizeEEService } from 'ontimize-web-ngx';
+import { Observable } from 'rxjs';
+import { StarsWarsResponseAdapter } from './star-wars-response-adapter';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class StarWarsService {
+@Injectable()
+export class StarsWarsService extends OntimizeEEService {
 
-  constructor() { }
+  constructor(protected injector: Injector) {
+    super(injector);
+  }
+
+  public query(kv?: Object, av?: Array<string>, entity?: string, sqltypes?: Object): Observable<any> {
+    const identifier = kv.valueOf()[Object.keys(kv)[0]];
+    let url = '';
+    if (Object.keys(kv).length === 0) {
+      url = 'https://swapi.dev/api/' + entity + '/?format=json';
+    } else {
+      url = 'https://swapi.dev/api/' + entity + '/' + identifier + '/?format=json';
+    }
+
+    return this.doRequest({
+      method: 'GET',
+      url: url,
+      options: {}
+    });
+  }
+
+  public configureAdapter() {
+    this.adapter = this.injector.get(StarsWarsResponseAdapter);
+  }
 }
