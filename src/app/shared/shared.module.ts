5ab1f1370c6b55be83edafb84ce100cffb2f8083
angel.herce <angel.herce@imatia.com>
2022-11-28 08:34:14 +0100

    Se ha añadido el método para calcular el interés anual en el módulo Shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 
+export function intRateMonthlyFunction (rowData: Array<any>): number {
+  return rowData["INTERESRATE"]/12;
+}
+
 @NgModule({
   imports: [
     OntimizeWebModule
