57a954d892c98665ac3e56659b4409518199d097
angel.herce <angel.herce@imatia.com>
2022-11-28 09:07:42 +0100

    Se ha añadido el componente con el detalle de cuentas

--- src/app/main/accounts/accounts-routing.module.ts
+++ src/app/main/accounts/accounts-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountsHomeComponent } from './accounts-home/accounts-home.component';
+import { AccountsDetailComponent } from './accounts-detail/accounts-detail.component';
 
 const routes: Routes = [{
   path:'',
   component: AccountsHomeComponent
+},
+{
+  path:':ACCOUNTID',
+  component: AccountsDetailComponent
 }];
 
 @NgModule({
--- src/app/main/accounts/accounts.module.ts
+++ src/app/main/accounts/accounts.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { AccountsHomeComponent } from './accounts-home/accounts-home.component';
-import { AccountNumberRenderComponent } from './accounts-home/account-number-render/account-number-render.component';
+import { AccountsDetailComponent } from './accounts-detail/accounts-detail.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [AccountsHomeComponent, AccountNumberRenderComponent],
+  declarations: [AccountsHomeComponent, AccountsDetailComponent],
   imports: [
     CommonModule,
+    SharedModule,
     OntimizeWebModule,
     AccountsRoutingModule
   ]
