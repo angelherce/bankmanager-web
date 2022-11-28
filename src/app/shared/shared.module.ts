4d9926cecb38e23c0989d159cc7ad35014bd2d4f
angel.herce <angel.herce@imatia.com>
2022-11-28 09:37:01 +0100

    Se ha creado un nuevo render con una imagen personalizada en el componente Home del módulo de Clientes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { AccountNumberRenderComponent } from '../main/accounts/accounts-home/account-number-render/account-number-render.component';
+import { CustomertypeColumnRendererComponent } from '../main/customers/customers-home/customertype-column-renderer/customertype-column-renderer.component';
 
 export function intRateMonthlyFunction (rowData: Array<any>): number {
   return rowData["INTERESRATE"]/12;
@@ -12,11 +13,13 @@ export function intRateMonthlyFunction (rowData: Array<any>): number {
     OntimizeWebModule
   ],
   declarations: [
-    AccountNumberRenderComponent
+    AccountNumberRenderComponent,
+    CustomertypeColumnRendererComponent
   ],
   exports: [
     CommonModule,
-    AccountNumberRenderComponent
+    AccountNumberRenderComponent,
+    CustomertypeColumnRendererComponent
   ]
 })
 export class SharedModule { }
