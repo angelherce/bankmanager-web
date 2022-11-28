5aa651516359022d3640c05c9f73e6e83faf2c18
angel.herce <angel.herce@imatia.com>
2022-11-28 10:08:46 +0100

    Se ha actualizado la tarjeta de empleados

--- src/app/shared/employee-card/employee-card.component.ts
+++ src/app/shared/employee-card/employee-card.component.ts
@@ -1,13 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewEncapsulation } from '@angular/core';
+import { OntimizeService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-employee-card',
   templateUrl: './employee-card.component.html',
-  styleUrls: ['./employee-card.component.css']
+  styleUrls: ['./employee-card.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    '[class.home-card]': 'true'
+  }
 })
 export class EmployeeCardComponent implements OnInit {
+  public employeeAmount: number;
 
-  constructor() { }
+  constructor(
+    private ontimizeService: OntimizeService,
+    private cd: ChangeDetectorRef,
+  ) {
+    this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('employees'));
+    this.ontimizeService.query(undefined, ['EMPLOYEEID'], 'employee').subscribe(
+      res => {
+        if (res.data && res.data.length) {
+          this.employeeAmount = res.data.length;
+        }else{
+          this.employeeAmount = undefined;
+        }
+
+        },
+      err => console.log(err),
+      () => this.cd.detectChanges()
+    );
+   }
 
   ngOnInit() {
   }
